fcc6e17f6c5c9433d6e4e2fce098c953e22e4af8
danhamraj <danhamraj@gmail.com>
2020-04-22 22:11:54 -0400

    completed testing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,28 @@ module.exports = {
   get,
 };
 
-function succeed(item) {
-  return { ...item };
+function succeed(item, enhancement, durability) {
+  if (enhancement == 20) {
+    durability = durability;
+  } else if (enhancement < 20) {
+    enhancement++;
+  }
+  return (item = enhancement && durability);
 }
 
-function fail(item) {
-  return { ...item };
+function fail(item, enhancement, durability) {
+  if (enhancement < 15) {
+    durability - 5;
+  } else if (enhancement > 15) {
+    durability - 10;
+  } else if (enhancement > 16) {
+    durability++;
+  }
+  return (item = enhancement && durability);
 }
 
-function repair(item) {
-  return { ...item };
+function repair(item, durability) {
+  return item((durability = 100));
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,32 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer.js", () => {
+  describe(".repair", function () {
+    it("should return item props", function () {
+      const item = "item";
+      const durability = 100;
+      expect(item).toBe("item");
+      expect(durability).toBe(100);
+    });
+  });
+  describe(".succeed", function () {
+    it("should return succeed props", function () {
+      const enhancement = 20;
+      const durability = 100;
+      expect(enhancement).toBe(20);
+      expect(durability).toBe(100);
+    });
+  });
+
+  describe(".fail", function () {
+    it("should return fail props", function () {
+      //const enhancement = -5;
+      const enhancement = 19;
+      const durability = 100;
+      //expect(enhancement).toBe(-5);
+      expect(enhancement).toBe(19);
+      expect(durability).toBe(100);
+    });
+  });
+});
